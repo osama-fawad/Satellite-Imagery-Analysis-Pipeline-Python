e8947525722bf98004591a58b4eef5dad039b8cb
osamafawad <osamafawad@gmail.com>
2024-12-28 23:26:39 +0100

    removing everthing

--- database/setup.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE spread_history (
-    id SERIAL PRIMARY KEY,
-    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    wti_price NUMERIC,
-    brent_price NUMERIC,
-    spread NUMERIC
-);
